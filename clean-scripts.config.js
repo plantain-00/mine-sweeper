58d3e9198152725ed0ca79a91a8e425f52ddc0a7
york yao <yaoao12306@outlook.com>
2017-08-07 23:13:22 +0800

    improve scripts

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -1,27 +1,33 @@
 module.exports = {
   build: [
-    `rimraf *.bundle-*.js *.bundle-*.css`,
-    `rimraf dist`,
-    `tsc`,
-    `file2variable-cli index.template.html -o variables.ts --html-minify`,
-    `tsc`,
-    `lessc index.less > index.css`,
-    `cleancss -o index.bundle.css index.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`,
-    `webpack --display-modules --config webpack.config.js`,
+    {
+      js: [
+        `rimraf dist`,
+        `file2variable-cli index.template.html -o variables.ts --html-minify`,
+        `tsc`,
+        `webpack --display-modules --config webpack.config.js`
+      ],
+      css: [
+        `lessc index.less > index.css`,
+        `cleancss -o index.bundle.css index.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`
+      ],
+      clean: `rimraf *.bundle-*.js *.bundle-*.css`
+    },
     `rev-static --config rev-static.config.js`,
     `sw-precache --config sw-precache.config.js --verbose`,
     `uglifyjs service-worker.js -o service-worker.bundle.js`
   ],
-  lint: [
-    `tslint "index.ts"`,
-    `standard "**/*.config.js"`,
-    `stylelint "index.less"`
-  ],
+  lint: {
+    ts: `tslint "index.ts"`,
+    js: `standard "**/*.config.js"`,
+    less: `stylelint "index.less"`
+  },
   test: [
     'tsc -p spec',
     'karma start spec/karma.config.js'
   ],
-  fix: [
-    `standard --fix "**/*.config.js"`
-  ]
+  fix: {
+    js: `standard --fix "**/*.config.js"`,
+    less: `stylelint --fix "index.less"`
+  }
 }
