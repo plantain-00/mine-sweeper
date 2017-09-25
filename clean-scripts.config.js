f96041d5a9ab21c2bf259ed2a4a5e17937624857
york yao <yaoao12306@outlook.com>
2017-09-25 16:58:36 +0800

    add postcss

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -13,7 +13,8 @@ module.exports = {
       ],
       css: [
         `lessc index.less > index.css`,
-        `cleancss -o index.bundle.css index.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`
+        `postcss index.css -o index.postss.css`,
+        `cleancss -o index.bundle.css index.postss.css ./node_modules/github-fork-ribbon-css/gh-fork-ribbon.css`
       ],
       clean: `rimraf *.bundle-*.js *.bundle-*.css`
     },
