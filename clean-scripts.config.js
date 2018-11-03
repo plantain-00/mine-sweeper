1d3bca6672bb29525c359c29be4cebf28bf57c65
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-03 00:58:33 +0000

    Deploy plantain-00/mine-sweeper to github.com/plantain-00/mine-sweeper.git:gh-pages

--- clean-scripts.config.js
+++ clean-scripts.config.js
@@ -42,7 +42,7 @@ module.exports = {
     export: `no-unused-export ${tsFiles} ${lessFiles}`,
     commit: `commitlint --from=HEAD~1`,
     markdown: `markdownlint README.md`,
-    typeCoverage: 'type-coverage -p . --at-least 91'
+    typeCoverage: 'type-coverage -p . --at-least 90'
   },
   test: [
     'tsc -p spec',
